479d229cc75cdd73fe273c979bd4bb5773c1baf0
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:42:29 +0300

    Добавьте файлы проекта.

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        if (!int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
+            return;
+        }
+
+        if (number == 5 || number == 10)
+        {
+            Console.WriteLine("Число либо равно 5, либо равно 10");
+        }
+        else
+        {
+            Console.WriteLine("Неизвестное число");
+        }
+    }
+}
